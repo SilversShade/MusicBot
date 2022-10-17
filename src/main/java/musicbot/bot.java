438f6c828dcc7099bf87f96cc3841f3467aefbfe
CBist <dsync@list.ru>
2022-10-17 23:29:48 +0500

    Added tools for working with the bot

--- src/main/java/musicbot/bot.java
+++ src/main/java/musicbot/bot.java
@@ -2,6 +2,9 @@ package musicbot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageCaption;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -13,7 +16,7 @@ import java.util.List;
 public class bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
-
+        // Обработка команд
     }
 
     @Override
@@ -24,6 +27,100 @@ public class bot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "5723529211:AAFYshIIkyFwyTI7RhH_Vjkr9zl8bmJYJW8";
+    }
+
+    // Отправка сообщений
+    public boolean sendMessage(String text, long chatId){
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            return false;
+        }
+        return true;
+    }
+    public boolean sendMessage(String text, long chatId, List<List<InlineKeyboardButton>> buttons){
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
+        keyboard.setKeyboard(buttons);
+        message.setReplyMarkup(keyboard);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            return false;
+        }
+        return true;
+    }
+    public boolean sendMessage(String text, long chatId, InlineKeyboardMarkup keyboard){
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+        message.setReplyMarkup(keyboard);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            return false;
+        }
+        return true;
+    }
+
+    // Редактирование сообщения
+    public boolean editMessage(String text, long chatId, int messageId){
+        EditMessageText message = new EditMessageText();
+        message.setChatId(chatId);
+        message.setText(text);
+        message.setMessageId(messageId);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            return false;
+        }
+        return true;
+    }
+    public boolean editMessage(String text, long chatId, int messageId, List<List<InlineKeyboardButton>> buttons){
+        EditMessageText message = new EditMessageText();
+        message.setChatId(chatId);
+        message.setText(text);
+        message.setMessageId(messageId);
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
+        keyboard.setKeyboard(buttons);
+        message.setReplyMarkup(keyboard);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            return false;
+        }
+        return true;
+    }
+    public boolean editMessage(String text, long chatId, int messageId, InlineKeyboardMarkup keyboard){
+        EditMessageText message = new EditMessageText();
+        message.setChatId(chatId);
+        message.setText(text);
+        message.setMessageId(messageId);
+        message.setReplyMarkup(keyboard);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            return false;
+        }
+        return true;
+    }
+
+    // Удаление сообщения
+    public boolean deleteMessage(long chatId, int messageId){
+        DeleteMessage message = new DeleteMessage();
+        message.setChatId(chatId);
+        message.setMessageId(messageId);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            return false;
+        }
+        return true;
     }
 }
 
