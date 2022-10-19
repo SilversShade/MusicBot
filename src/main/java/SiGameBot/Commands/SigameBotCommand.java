8a2aedcef2d79c8bb41dac7fb2cda0e48d0a211b
Alexander Gorbatov <shantem965@mail.ru>
2022-10-19 17:54:30 +0500

    Added /begin command processing

--- src/main/java/SiGameBot/Commands/SigameBotCommand.java
+++ src/main/java/SiGameBot/Commands/SigameBotCommand.java
@@ -1,15 +1,14 @@
 package SiGameBot.Commands;
 
 import SiGameBot.SigameBot;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public abstract class SigameBotCommand extends BotCommand {
 
     protected SigameBot bot;
+
     public SigameBotCommand(String command, String description, SigameBot bot) {
         super(command, description);
         this.bot = bot;
